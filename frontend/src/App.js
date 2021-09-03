109bd836348e36f2e791a958b1d7eed374ce0bee
tuanthanh2067 <tuanthanh2067@gmail.com>
2021-09-03 12:37:10 -0400

    initial set up for client side

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <div className="App">Test</div>
+    </ApolloProvider>
+  );
+}
+
+export default App;
